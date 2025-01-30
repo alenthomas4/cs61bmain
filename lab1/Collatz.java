7a2d3dd0a9a9f31608fe3c41f5df6456e9630c8f
Alen Thomas <alen.calgary@gmail.com>
2025-01-29 22:00:44 -0700

    updated collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -6,17 +6,25 @@ public class Collatz {
     /** Buggy implementation of nextNumber! */
     // what is going on
     public static int nextNumber(int n) {
-        if (n  == 128) {
+       /* if (n  == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
             return n * 2;
+        }*/
+        if (n%2 != 0) {
+            return (3*n + 1);
+        }
+        else {
+            return (n/2);
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
+        System.out.println(nextNumber(n));
+
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
