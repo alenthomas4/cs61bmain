e129536b93127eb42d0d1c2fa85f1325917f388c
Alen Thomas <alen.calgary@gmail.com>
2025-01-29 22:06:36 -0700

    updated

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -23,9 +23,7 @@ public class Collatz {
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(nextNumber(n));
-
-        //System.out.print(n + " ");
+        System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
